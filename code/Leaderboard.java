1661ee835f1b37c54255d8143d54787c402caea0
nsimon99 <59479117+nsimon99@users.noreply.github.com>
2022-02-23 23:01:39 -0500

    override toString and lamba for sorting

--- code/Leaderboard.java
+++ code/Leaderboard.java
@@ -19,37 +19,12 @@ public class Leaderboard implements ILeaderBoardService {
     }
     
     
-    @Override
-    public ArrayList<Player> sortList(){
-        ArrayList<Player> result = new ArrayList<Player>();
-        int length = board.size();
-        // Sorting using a single loop
-        for (int j = 0; j < length - 1; j++) {
- 
-            // Type Conversion of char to int.
-            int d1 = board.get(j).getScore();
-            int d2 = board.get(j + 1).getScore();
- 
-            // Comparing the ascii code.
-            if (d1 > d2) {
- 
-                // Swapping of the characters
-                Player temp =  board.get(j);
-                Player temp2 =  board.get(j + 1);
-                
-                result.add(j, temp2);
-                result.add(j + 1, temp);
-                j = -1;
-            }
-        }
- 
-        return result;
-    }
-
+   
 
     @Override
      public List<Player> topN (int n){
-        Collections.sort(board, (p1, p2) -> Integer.compare(p1.getScore(), p2.getScore()));
+        Collections.sort(board,(p1,p2)->{return p2.getScore().compareTo(p1.getScore());});  
+
          if (n > board.size()) {
           throw new IllegalArgumentException("Requested number bigger than board");
         }
@@ -57,6 +32,8 @@ public class Leaderboard implements ILeaderBoardService {
          List<Player> leaders = board.subList(0, n);
          return leaders;
      }
+     
+    
     
   public String listToString(List<Player> list) {
     StringBuilder result = new StringBuilder();
