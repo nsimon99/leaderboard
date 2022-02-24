2adc24fd18bdf783970ee31901f7e5857bd4862b
nsimon99 <59479117+nsimon99@users.noreply.github.com>
2022-02-23 23:02:59 -0500

    create toString

--- code/Player.java
+++ code/Player.java
@@ -1,40 +1,62 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Comparator;
-import java.util.Collections;
 
-public class Main
-{
-    public static void main(String[] args) {
-        Player p1 = new Player("neha", 100,100);
-        Player p2 = new Player("george", 150,110);
-        Player p3 = new Player("p3", 150,120);
-        Player p4 = new Player("p4", 200,130);
-        Player p5 = new Player("p5", 250,140);
-        Player p6 = new Player("p6", 300,150);
-        Player p7 = new Player("p7", 350,160);
-        Player p8 = new Player("p8", 400,170);
-        Player p9 = new Player("p9", 450,180);
-        Player p10 = new Player("p10", 500,190);
-        Player p11 = new Player("p11", 600,200);
-        ArrayList<Player> players= new ArrayList<>();
-        players.add(p1);
-        players.add(p2);
-        players.add(p3);
-        players.add(p4);
-        players.add(p5);
-        players.add(p6);
-        players.add(p7);
-        players.add(p8);
-        players.add(p9);
-        players.add(p10);
-        players.add(p11);
-        Leaderboard lb = new Leaderboard(players);
+/* 
+* Build a player for a leader board
+*/
 
-        //System.out.print(lb.listToString(players));
-        List<Player> result = new ArrayList<Player>(lb.topN(5));
-        System.out.println(lb.listToString(result));
-        
+public class Player {
+    public String username;
+    public Integer score;
+    public int index;
+    
+    /**
+   * constructor for player.
+   *
+   * @param username    represents username of player
+   * @param score       represents score player has
+   * @param index       represents index of plater
+   */
+  public Player(
+       String username,
+       Integer score,
+       int index) 
+       {
+    this.username = username;
+    this.score = score;
+    this.index = index;
+       }
+       
+       /* gets the score of each player 
+       */
+       
+     public Integer getScore() {
+         return this.score;
+     }
+     
+     /* gets the name of each player 
+       */
+       
+     public String getName() {
+         return this.username;
+     }
+     
+     /* gets the index of each player 
+       */
+       
+     public int getIndex() {
+         return this.index;
+     }
+     
+     /* gets the string 
+       */
+       
+       
+     @Override
+     public String toString() {
+         return this.username + " " + String.valueOf(this.score) + " " + String.valueOf(this.index);
+     }
+   
+   
 
-    }
 }
